b85d08ca843e585eabe5f96589a1ebeb1a6c35f5
Ahmer Maqsood Hashmi <hashmi.ahmermaqsood@teradata.com>
2019-05-21 14:54:44 +0500

    Updated java code
    Updated java code

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -5,8 +5,9 @@ public class JenkinsTest {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7,8,9};
         
+        System.out.println("Table of 2 : ");
         for(int num: arr){
-            System.out.println(num);
+            System.out.println(2 + " * " + num + " = " + (2*num));
         }
     }
 
